6be3fabb7498d286ef98679c734f27b61f6a0f05
hari krishna <gadiharikrishna@gmail.com>
2019-04-21 17:22:52 +0530

    Intitial

--- myScript.js
+++ myScript.js
@@ -1,7 +1,7 @@
 
 
 var uluru;
-var map, infoWindow;
+var map,infowindow,marker;
 function loadfire(){
 
       // Initialize Firebase
@@ -16,95 +16,67 @@ function loadfire(){
   };
   firebase.initializeApp(config);
 
+
+ contentString='<div><img src="images/my.png" alt="No latest Detection" style="width:300px;height:300px;"></div>'
+
  var ref = firebase.database().ref('/cameras/camera1');
  //console.log(ref)
  ref.once('value',function(snapshot){
    console.log(snapshot.val()["loc"])
    if(snapshot.val()["loc"] != null){
-      map.setCenter(snapshot.val()["loc"    ]);
-     var marker = new google.maps.Marker({position:snapshot.val()["loc"] , map: map});
+      map.setCenter(snapshot.val()["loc"]);
+      marker = new google.maps.Marker({position:snapshot.val()["loc"] , animation: google.maps.Animation.DROP,map: map});
+     marker.addListener('click', togglepop);
+     infowindow = new google.maps.InfoWindow({
+          content: contentString
+    });
+     var trafficLayer = new google.maps.TrafficLayer();
+     trafficLayer.setMap(map);
    }
  });
 
-ref.on('value',function (snapshot){
-   console.log(snapshot.val()["objects"])
+ref.once('value',function (snapshot){
+   //console.log(snapshot.val()["objects"]['person']['count'])
+   data  = snapshot.val()["objects"];
+   for (key in data){
+
+      if(key==='person'){
+         //console.log(snapshot.val()["objects"][key]['count'])
+         document.getElementById('value1').innerHTML=data[key]['count']
+         document.getElementById('time1').innerHTML="Last updated: "+data[key]['timestamp']
+      }
+      else if (key==='car') {
+        document.getElementById('value2').innerHTML=data[key]['count']
+        document.getElementById('time2').innerHTML="Last updated: "+data[key]['timestamp']
+
+      }
+    else if (key==='bike') {
+      document.getElementById('value3').innerHTML=data[key]['count']
+      document.getElementById('time3').innerHTML="Last updated: "+data[key]['timestamp']
+
+    }
+   }
 });
 
 }
 
+function togglepop() {
+        //alert("clicked")
+        infowindow.open(map,marker)
+      }
 
 function initMap() {
         // Styles a map in night mode.
+        console.log("hi")
         loadfire();
          uluru = {lat: 17.726367, lng: 83.320782};
       // The marker, positioned at Uluru
-         map = new google.maps.Map(document.getElementById('map'), {
+        map = new google.maps.Map(document.getElementById('map'), {
           center: uluru,
           zoom: 18,
           styles: [
-            {elementType: 'geometry', stylers: [{color: '#242f3e'}]},
-            {elementType: 'labels.text.stroke', stylers: [{color: '#242f3e'}]},
-            {elementType: 'labels.text.fill', stylers: [{color: '#746855'}]},
-            {
-              featureType: 'administrative.locality',
-              elementType: 'labels.text.fill',
-              stylers: [{color: '#d59563'}]
-            },
-            {
-              featureType: 'poi.school',
-              elementType: 'labels.text.fill',
-              stylers: [{color: '#d59563'}]
-            },
-            {
-              featureType: 'road',
-              elementType: 'geometry',
-              stylers: [{color: '#38414e'}]
-            },
-            {
-              featureType: 'road',
-              elementType: 'geometry.stroke',
-              stylers: [{color: '#212a37'}]
-            },
-            {
-              featureType: 'road',
-              elementType: 'labels.text.fill',
-              stylers: [{color: '#9ca5b3'}]
-            },
-            {
-              featureType: 'road.highway',
-              elementType: 'geometry',
-              stylers: [{color: '#746855'}]
-            },
-            {
-              featureType: 'road.highway',
-              elementType: 'geometry.stroke',
-              stylers: [{color: '#1f2835'}]
-            },
-            {
-              featureType: 'road.highway',
-              elementType: 'labels.text.fill',
-              stylers: [{color: '#f3d19c'}]
-            },
-            {
-              featureType: 'transit',
-              elementType: 'geometry',
-              stylers: [{color: '#2f3948'}]
-            },
-            {
-              featureType: 'transit.station',
-              elementType: 'labels.text.fill',
-              stylers: [{color: '#d59563'}]
-            },
-
 
           ]
         });
-
-
-
-
-
-
-
 
       }
